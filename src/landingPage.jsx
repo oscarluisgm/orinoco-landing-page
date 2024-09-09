6043ff1f6ce96f38537a44d7c8a24d666678688e
unknown <oscarluisgm2@gmail.com>
2024-09-09 19:06:11 -0400

    ok

---
+++ src/landingPage.jsx
@@ -0,0 +1,38 @@
+/* import  */
+
+export const LandingOrinoco=()=>{
+
+    return <>
+        <div className="nav-bar d-flex justify-content-end gap-3">
+            <button>Acerca de nosotros</button>
+            <button>Reserva ya</button>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className="col hero-section">
+                    
+                    <h1>Viaje con comodidad hasta Trinidad & Tobago</h1>
+                    <h6>Tenemos las mejores embarcaciones manejadas por pilotos con vasta experiencia para realizar viajes marítimos más seguros</h6>
+                    <button>Compra tu boleto</button>
+                    
+                    {/* <img href={}/> */}
+                </div>
+            </div>
+            <div className="row">
+                <div className="col features-benefits"></div>
+            </div>
+            <div className="row">
+                <div className="col objection-faq"></div>
+            </div>
+            <div className="row">
+                <div className="col more-social-proof"></div>
+            </div>
+            <div className="row">
+                <div className="col footer"></div>
+            </div>
+        </div>    
+    
+    </>
+
+
+}
